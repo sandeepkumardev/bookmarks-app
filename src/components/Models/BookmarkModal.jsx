95455ff3ba1587db9e3b2a886326a5872651de7e
sandeep-kumar <sandeepypb@gmail.com>
2023-12-18 12:13:39 +0530

    feat: add bookmark icon

--- src/components/Models/BookmarkModal.jsx
+++ src/components/Models/BookmarkModal.jsx
@@ -11,17 +11,14 @@ import {
   Select,
   Spacer,
 } from "@chakra-ui/react";
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { AppActions } from "../../context/ActionProvider";
 import { AppStore } from "../../context/StoreProvider";
 
 function BookmarkModal() {
   const { isBmOpen, bmModalHandler, ctModalHandler } = useContext(AppActions);
   const { addBookmark, items } = useContext(AppStore);
-
-  const [icon] = useState(
-    "https://www.google.com/images/branding/googleg/1x/googleg_standard_color_128dp.png"
-  );
+  const [icon, setIcon] = useState("");
   const [url, setUrl] = useState("");
   const [title, setTitle] = useState("");
   const [selectedCategory, setSelectedCategory] = useState({
@@ -34,6 +31,42 @@ function BookmarkModal() {
       alert("Please provide all fields!");
       return;
     }
+
+    var inp = url;
+    var http = url.search("https://");
+
+    if (http == "-1") {
+      inp = "https://" + url;
+    }
+
+    var regexp =
+      /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+
+    if (regexp.test(inp)) {
+      var urlData = new URL(inp);
+      var domain = urlData.origin;
+
+      setUrl(urlData.href);
+      let imageUrl = `${domain}/favicon.ico`;
+
+      function imageExists(url) {
+        var img = new Image();
+        img.onload = function () {
+          setIcon(imageUrl);
+        };
+        img.onerror = function () {
+          setIcon(`http://www.google.com/s2/favicons?domain=${domain}`);
+        };
+        img.src = url;
+      }
+      imageExists(imageUrl);
+    } else {
+      console.log("Url is invalid!");
+    }
+  };
+
+  useEffect(() => {
+    if (!icon) return;
 
     const newItem = {
       title,
@@ -48,7 +81,7 @@ function BookmarkModal() {
     setTitle("");
     setSelectedCategory("");
     bmModalHandler();
-  };
+  }, [icon]);
 
   return (
     <>
